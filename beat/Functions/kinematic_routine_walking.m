6d83adec2848eb54083b11e1b16a03f98d38e97e
Anthony Remazeilles <anthony.remazeilles@tecnalia.com>
2020-04-29 00:20:46 +0200

    Add output folder name

--- beat/Functions/kinematic_routine_walking.m
+++ beat/Functions/kinematic_routine_walking.m
@@ -1,21 +1,22 @@
-function [mROM CoV]=kinematic_routine_walking(fileName,PlatformData)
+function [mROM CoV]=kinematic_routine_walking(fileName, PlatformData, outFolder)
 % kinematic_routine_walking is a function that gives the kinematic parameters related to the stepping protocols
 %Required Octave pkg: signal, statistics, linear-algebra
 %[mROM CoV]=kinematic_routine_walking(fileName,PlatformData)
 % INPUT:
-%       - fileName must be a .csv file with this structure containing joint angle data: 
+%       - fileName must be a .csv file with this structure containing joint angle data:
 %         1st column: time-stamp (units: milliseconds).
 %         1st row: header with M+1 labels  were  M = number of angles considered.
 %         From 2nd row: angle values, each corresponding to one time sample (units: degrees).
 %       - PlatformData is a file containing the data extracted by the platform.
+%       - outFolder: folder path where result should be stored
 % OUTPUT:
 %       - mROM is the average ROM across repetitions of the same task or across strides based on the type of protocol. Dimension 2xNa. First line: name of angles, Second line: mROM values
 %       - CoV is the variability coefficienty related to the ROM values. Dimension 2xNa. First line: name of angles, Second line: mROM values
-%       
+%
 % $Author: J. TABORRI, v1 - 03/Apr/2020$ (BEAT project)
 
 
-data=csv2cell(fileName,";"); 
+data=csv2cell(fileName,";");
 platformdata=csv2cell(PlatformData, ";");
 time_vector_data=cell2mat(data(2:end,1)); %%extract time vector from data
 angle_matrix=cell2mat(data(2:end,2:end)); %%extract angle matrix from data
@@ -28,50 +29,50 @@ fs_angle=round(1.0/mean(delta_t_angle));
 delta_t_platform=diff(time_vector_platform,1);
 fs_platform=round(1.0/mean(delta_t_platform))
 
-if (fs_angle~=fs_platform)   
-for a=1:angle_number
- angle_matrix(:,a)=resample(angle_matrix(:,a),fs_platform,fs_angle);  %%make two signals with same sampling frequency
-end
+if (fs_angle~=fs_platform)
+  for a=1:angle_number
+    angle_matrix(:,a)=resample(angle_matrix(:,a),fs_platform,fs_angle);  %%make two signals with same sampling frequency
+  end
 else
-fprintf('Same sample frequency') 
+  fprintf('Same sample frequency')
 end
 
 %%angle partitioning
 if(platformdata{1,2}==1 || platformdata{1,2}==2) %% 1 and 2 represent the two stepping protocol
-event_1r=cell2mat(platformdata(:,21)); %%21st column of platformdata represents the stride identification performed by the pressure matrix embedded in the platform fro right side
- event_r=find(event_1r==1); 
-event_1l=cell2mat(platformdata(:,23)); %%23rd column of platformdata represents the stride identification performed by the pressure matrix embedded in the platform fro right side
- event_l=find(event_1l==1);
- else
- fprintf('You have tried to lunch EMG_routine with a wrong protocol') 
- return;
-endif
+  event_1r=cell2mat(platformdata(:,21)); %%21st column of platformdata represents the stride identification performed by the pressure matrix embedded in the platform fro right side
+  event_r=find(event_1r==1);
+  event_1l=cell2mat(platformdata(:,23)); %%23rd column of platformdata represents the stride identification performed by the pressure matrix embedded in the platform fro right side
+  event_l=find(event_1l==1);
+else
+  fprintf('You have tried to lunch EMG_routine with a wrong protocol')
+  return;
+end
 
 angle_r=find(~cellfun(@isempty,strfind(angle_label,'r_')));
 angle_l=find(~cellfun(@isempty,strfind(angle_label,'l_')));
 
 
- for e=1:length(event_r)-1
-   for a=1:length(angle_r)
-   angle_matrix_part_r(:,a,e)=eventsnormalize(angle_matrix(:,angle_r(a)),[round(event_r(e)) round(event_r(e+1))],101); %%normalize the frames within each event
- end
-end  
+for e=1:length(event_r)-1
+  for a=1:length(angle_r)
+    angle_matrix_part_r(:,a,e)=eventsnormalize(angle_matrix(:,angle_r(a)),[round(event_r(e)) round(event_r(e+1))],101); %%normalize the frames within each event
+  end
+end
 
 for ee=1:length(event_l)-1
-   for aa=1:length(angle_l)
-   angle_matrix_part_l(:,aa,ee)=eventsnormalize(angle_matrix(:,angle_l(aa)),[round(event_l(ee)) round(event_l(ee+1))],101); %%normalize the frames within each event
+  for aa=1:length(angle_l)
+    angle_matrix_part_l(:,aa,ee)=eventsnormalize(angle_matrix(:,angle_l(aa)),[round(event_l(ee)) round(event_l(ee+1))],101); %%normalize the frames within each event
  end
-end  
+end
 
 for i=1:size(angle_matrix_part_r,3)
   for a=1:length(angle_r)
-  ROM_r(i,a)=abs(max(angle_matrix_part_r(:,a,i))-min(angle_matrix_part_r(:,a,i)));  %%compute the range of motion for each stride during the task
+    ROM_r(i,a)=abs(max(angle_matrix_part_r(:,a,i))-min(angle_matrix_part_r(:,a,i)));  %%compute the range of motion for each stride during the task
   end
 end
 
 for ii=1:size(angle_matrix_part_l,3)
   for aa=1:length(angle_l)
-  ROM_l(ii,aa)=abs(max(angle_matrix_part_l(:,aa,ii))-min(angle_matrix_part_r(:,aa,ii)));  %%compute the range of motion for each stride during the task
+    ROM_l(ii,aa)=abs(max(angle_matrix_part_l(:,aa,ii))-min(angle_matrix_part_r(:,aa,ii)));  %%compute the range of motion for each stride during the task
   end
 end
 
@@ -93,8 +94,9 @@ angle_label2=cat(2,angle_label(angle_r),angle_label(angle_l));
 %%save mROM value in .yaml file
 [aaa, name, extension]=fileparts(PlatformData);
 name2=regexprep(name,'_PlatformData','');
-file_id=fopen(strcat(pwd,"/", name2, "_mROM", ".yaml"),'w'); %%open file to write into
-fprintf(file_id, "type: 'vector' 'measure unit: °\' \n");
+file_id=fopen(strcat(outFolder,"/", name2, "_mROM", ".yaml"),'w'); %%open file to write into
+fprintf(file_id, "type: 'vector'\n");
+fprintf(file_id, "'measure unit: %%' \n");
 label_str="angle_label: [";
 for i=1:size(angle_label2,2)
   label_str=sprintf("%s",label_str,char(angle_label2(i)));
@@ -118,8 +120,9 @@ fclose(file_id)
 
 
 %%save CoV value in .yaml file
-file_id=fopen(strcat(pwd,"/", name2, "_CoV", ".yaml"),'w'); %%open file to write into
-fprintf(file_id, "type: 'vector' 'measure unit: %%' \n");
+file_id=fopen(strcat(outFolder,"/", name2, "_CoV", ".yaml"),'w'); %%open file to write into
+fprintf(file_id, "type: 'vector'\n");
+fprintf(file_id, "'measure unit: %%' \n");
 label_str="angle_label: [";
 for i=1:size(angle_label2,2)
   label_str=sprintf("%s",label_str,char(angle_label2(i)));
--- run_kinematic_walking.m
+++ run_kinematic_walking.m
@@ -4,8 +4,11 @@ printf ("current directory: %s\n", pwd())
 addpath("beat/Functions") %%beat is the folder containing all the functions needed for the BEAT project PI algos
 addpath("beat/AdditionalFunctions")
 arg_list = argv ();
-if nargin != 2
-  printf ("ERROR: There should be 2 args. A .csv file containing the angles data, a .csv file containing the platform data\n");
+if nargin != 3
+  printf ("ERROR: Provided %d parameters, but expecting 3\n", nargin)
+  printf ("Usage:\n")
+  printf ("\t run_kinematic_walking jointAngle.csv platformData.csv out_folder\n")
+  exit(1)
 endif
 
 printf ("csv file: %s\n", arg_list{1});
@@ -14,4 +17,4 @@ printf ("platform data: %s\n", arg_list{2});
 pkg load io
 pkg load signal
 
-[mROM CoV]=kinematic_routine_walking(arg_list{1}, arg_list{2})
+[mROM CoV]=kinematic_routine_walking(arg_list{1}, arg_list{2}, arg_list{3})
