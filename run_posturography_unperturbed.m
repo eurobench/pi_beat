fa59b012fb5021f3d8d89972938e3fea9ce6903b
juritaborri <juri.taborri@unitus.it>
2020-04-29 16:07:56 +0200

    Adjust scripts and move outside the folder

---
+++ run_posturography_unperturbed.m
@@ -0,0 +1,16 @@
+#!/usr/bin/octave -qf
+printf ("current directory: %s\n", pwd())
+addpath("beat/Functions") %%beat is the folder containing all the functions needed for the BEAT project PI algos
+addpath("beat/AdditionalFunctions")
+arg_list = argv ();
+if nargin != 2
+  printf ("ERROR: Provided %d parameters, but expecting 2\n", nargin)
+  printf ("Usage:\n")
+  printf ("\t run_posturography_unperturbed platformData.csv out_folder\n")
+  exit(1)
+endif
+
+printf ("platform data: %s\n", arg_list{1});
+
+
+[PL PL_AP PL_ML EA]=posturographic_routine_unperturbed(arg_list{1},arg_list{2})
---
+++ run_sinsuoidalperturbation.m
@@ -0,0 +1,17 @@
+#!/usr/bin/octave -qf
+printf ("current directory: %s\n", pwd())
+addpath("beat/Functions") %%beat is the folder containing all the functions needed for the BEAT project PI algos
+addpath("beat/AdditionalFunctions")
+arg_list = argv ();
+if nargin != 3
+  printf ("ERROR: Provided %d parameters, but expecting 3\n", nargin)
+  printf ("Usage:\n")
+  printf ("\t run_sinusoidalperturbation jointAngle.csv platformData.csv out_folder\n")
+  exit(1)
+endif
+
+printf ("csv file: %s\n", arg_list{1});
+printf ("platform data: %s\n", arg_list{2});
+
+
+[G phi]=sinusoidal_perturbation(arg_list{1}, arg_list{2}, arg_list{3})
