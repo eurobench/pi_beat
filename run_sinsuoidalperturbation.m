6acea9ca4208ee62938cf7726ef0664ef05271a8
juritaborri <63415341+juritaborri@users.noreply.github.com>
2020-04-29 15:56:02 +0200

    Adjust scripts and move outside the folder

---
+++ run_sinsuoidalperturbation.m
@@ -0,0 +1,12 @@
+printf ("current directory: %s\n", pwd())
+addpath("beat") %%beat is the folder containing all the functions needed for the BEAT project PI algos
+arg_list = argv ();
+if nargin != 2
+  printf ("ERROR: There should be 2 args. A .csv file containing the angle data, a .csv file containing the platform data\n");
+endif
+
+printf ("csv file: %s\n", arg_list{1});
+printf ("platform data: %s\n", arg_list{2});
+
+
+[G phi]=sinusoidal_perturbation(arg_list{1}, arg_list{2})
